9369754782f320cdc77d7495c575a8919b6ef8b9
Jonathan <jdlo@ualberta.ca>
2023-01-11 17:36:25 -0700

    Add github request

--- script.py
+++ script.py
@@ -5,4 +5,8 @@ print("Version of \"requests\" is: " + req.__version__)
 
 #Get the google homepage
 response = req.get("https://www.google.com")
-print("Got code " + str(response.status_code))
+print(response.status_code)
+
+#Print own source code from github
+ghResponse = req.get("https://raw.githubusercontent.com/jd-lo/CMPUT-404-Lab-1/main/script.py")
+print(ghResponse.content)
